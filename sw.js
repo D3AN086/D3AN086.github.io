f43524eccb2b8c94aa73e5ad0d9633fd0440db30
D3AN086 <153862135+D3AN086@users.noreply.github.com>
2026-09-22 22:59:26 +0100

    Unregister stale service worker so login is not blocked

--- sw.js
+++ sw.js
@@ -1,2 +1,8 @@
-self.addEventListener('install', e => { self.skipWaiting(); });
-self.addEventListener('activate', e => { e.waitUntil(self.clients.claim()); });
+self.addEventListener('install', function(e){ self.skipWaiting(); });
+self.addEventListener('activate', function(e){
+  e.waitUntil((async function(){
+    var keys = await caches.keys();
+    await Promise.all(keys.map(function(k){ return caches.delete(k); }));
+    await self.registration.unregister();
+  })());
+});
